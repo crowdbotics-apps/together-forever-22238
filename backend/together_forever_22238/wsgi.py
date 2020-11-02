8419f20e89a1e467486f8a66f9fea2df942fdea3
Crowdbotics <team@crowdbotics.com>
2020-11-02 23:51:52 +0000

    Update models

--- backend/together_forever_22238/wsgi.py
+++ backend/together_forever_22238/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'together_forever_22238.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "together_forever_22238.settings")
 
 application = get_wsgi_application()
